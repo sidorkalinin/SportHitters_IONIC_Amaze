b8ef45c7ed80c487c4afaeeccd9e0996496b3927
rohanbhuri <rohanbhuri@gmail.com>
2017-09-23 18:12:19 +0530

    home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, ModalController} from 'ionic-angular';
 
-import { FavouritesPage } from './../favourites/favourites';
-import { LoginPage } from './../login/login';
+import {FavouritesPage} from './../favourites/favourites';
+import {LoginPage} from './../login/login';
 //import {TabsPage} from './../tabs/tabs';
 
-import { SportsProvider } from '../../providers/sports/sports';
+import {SportsProvider} from '../../providers/sports/sports';
 
 
 @Component({
@@ -16,7 +16,6 @@ export class HomePage {
     loggedUser;
 
     sportsList = [];
-
     constructor(public navCtrl: NavController, public modalCtrl: ModalController, public sportService: SportsProvider) {
         this.loggedUser = localStorage.getItem("loggedUser");
         if (this.loggedUser == null) {
@@ -24,27 +23,20 @@ export class HomePage {
         }
         //get all the sport list and games 
         this.sportService.getAllSports().subscribe(response => {
-            console.log(response);
-            for (let item of response) {
-                console.log(item);
-            }
-            // if (response) {
-            //     for (var index = 0; index < response.length; index++) {
-            //         alert(response[index]);
-            //         let sport = {
-            //             name: response[index],
-            //             games: []
-            //         };
-            //         this.sportService.oddTypeGame(sport).subscribe(response => {
-            //             if (response) {
-            //                 sport.games.push(response);
-            //             }
-            //         });
-            //         this.sportsList.push(sport);
-            //     }
-            // }
+            if (response)
+                for (var index = 0; index < Object.keys(response).length; index++) {
+                    let sport = {
+                        name: response[index],
+                        games: []
+                    };
+                    this.sportService.oddTypeGame(response[index]).subscribe(response => {
+                        if (response) {
+                            sport.games.push(response);
+                        }
+                    })
+                    this.sportsList.push(sport);
+                }
         });
-
     }
 
 
